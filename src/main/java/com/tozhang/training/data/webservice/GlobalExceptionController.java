92fa681bcb8a6a0920dc318452c7016606f55288
Zhang <tozhang@was-pverma11.corp.microstrategy.com>
2019-05-13 16:58:02 -0400

    update global exception handler

--- src/main/java/com/tozhang/training/data/webservice/GlobalExceptionController.java
+++ src/main/java/com/tozhang/training/data/webservice/GlobalExceptionController.java
@@ -2,6 +2,7 @@ package com.tozhang.training.data.webservice;
 
 import com.tozhang.training.util.IDMResponse;
 import com.tozhang.training.util.ServiceRuntimeException;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionController extends Throwable {
-
+    private static final Logger logger = Logger.getLogger(GlobalExceptionController.class);
     @ExceptionHandler(value = ServiceRuntimeException.class)
     public ResponseEntity<Object> exception(ServiceRuntimeException exception) {
-        return new IDMResponse().Wrong(HttpStatus.BAD_REQUEST,"Invalid Access Token");
+        logger.error("Globally caught exception need to solve later");
+        exception.printStackTrace();
+        return new IDMResponse().Wrong(HttpStatus.BAD_REQUEST,"There is something wrong with our server");
     }
 }
 
